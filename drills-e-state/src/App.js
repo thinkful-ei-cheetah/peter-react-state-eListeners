351d0e0585b4d61ff0f36acde1ed4dc8bad317d3
Peter Pae <paendabear@gmail.com>
2019-05-28 19:04:40 -0400

    drills and app add

---
+++ drills-e-state/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import HelloWorld from './state-drills/helloWorld';
+import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun';
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hello World</h2>
+        <HelloWorld />
+        <h2>Bomb</h2>
+        <Bomb />
+        <h2>Roulette Gun</h2>
+        <RouletteGun bulletInChamber={6} />
+      </div>
+    );
+  }
+}
+
+export default App;
